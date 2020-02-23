98afb6277333e4c2adcb67c2f7fea6f49367b2d8
Stefaan <safwebdev@gmail.com>
2020-02-23 23:27:40 +0800

    Setup UI

---
+++ src/components/Balance.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Balance = () => {
+    return (
+        <>
+            <h4>Your Balnce</h4>
+            <h1 cl="balance">$0.00</h1>
+        </>
+    )
+}
